e894823379571de4913cc1ef97fe45ebffec2a24
sean <31554433+siriusfinex@users.noreply.github.com>
2019-04-24 11:11:02 +0800

    add obfs-simple

--- run.js
+++ run.js
@@ -25,7 +25,7 @@ if(runParams.indexOf('python') >= 0) {
   const tempPassword = 'qwerASDF' + Math.random().toString().substr(2, 8);
   shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', tempPassword, '-t', '600', '--fast-open', '--manager-address', ssConfig ]);
 } else {
-  shadowsocks = spawn('ss-manager', [ '-m', method, '-t', '600', '--fast-open', '-u', '--manager-address', ssConfig ]);
+  shadowsocks = spawn('ss-manager', ['--plugin', 'obfs-server', '--plugin-opts', '"obfs=http"', '-m', method, '-t', '600', '--fast-open', '-u', '--manager-address', ssConfig ]);
 }
 
 shadowsocks.stdout.on('data', (data) => {
